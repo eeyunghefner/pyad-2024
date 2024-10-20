4b1ed443de8daf5ef3695e355aba358df4fecb24
eeyunghefner <kravnik.03@gmail.com>
2024-10-21 00:20:06 +0300

    Update basic_math.py

--- lab0/basic_math.py
+++ lab0/basic_math.py
@@ -7,8 +7,19 @@ def matrix_multiplication(matrix_a, matrix_b):
     Задание 1. Функция для перемножения матриц с помощью списков и циклов.
     Вернуть нужно матрицу в формате списка.
     """
-    # put your code here
-    pass
+    rows_m1, cols_m1 = len(matrix_a), len(matrix_a[0])
+    rows_m2, cols_m2 = len(matrix_b), len(matrix_b[0])
+    if cols_m1 != rows_m2:
+        return "Умножение матриц невозможно"
+    result = [[0 for _ in range(cols_m2)] for _ in range(rows_m1)]
+    
+    for i in range(rows_m1):
+        for j in range(cols_m2):
+            for k in range(cols_m1):
+                result[i][j] += matrix_a[i][k] * matrix_b[k][j]
+    
+    return result
+  
 
 
 def functions(a_1, a_2):
