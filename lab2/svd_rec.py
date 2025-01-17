94f3b6d2b182fdcd9d3cfd9961cc44c83d80fabd
eeyunghefner <kravnik.03@gmail.com>
2025-01-17 13:59:03 +0300

    Update svd_rec.py

--- lab2/svd_rec.py
+++ lab2/svd_rec.py
@@ -8,20 +8,33 @@ from surprise import accuracy
 
 
 def ratings_preprocessing(df: pd.DataFrame) -> pd.DataFrame:
-    """Функция для предобработки таблицы Ratings.scv"""
+    ratings = df[df['Book-Rating'] != 0.0]
 
-    pass
+    books_with_one_review = ratings['ISBN'].value_counts()[ratings['ISBN'].value_counts() == 1].index
+    users_with_one_review = ratings['User-ID'].value_counts()[ratings['User-ID'].value_counts() == 1].index
+
+    filtered_rating_df = ratings[~ratings['ISBN'].isin(books_with_one_review) & ~ratings['User-ID'].isin(users_with_one_review)]
+
+    return filtered_rating_df
 
 
 def modeling(ratings: pd.DataFrame) -> None:
-    """В этой функции нужно выполнить следующие шаги:
-    1. Разбить данные на тренировочную и обучающую выборки
-    2. Обучить и протестировать SVD
-    3. Подобрать гиперпараметры (при необходимости)
-    4. Сохранить модель"""
+    reader = Reader(rating_scale=(1, 10))
+    ratings_data = Dataset.load_from_df(ratings[["User-ID", "ISBN", "Book-Rating"]], reader)
 
-    # ...
-    svd = SVD()
-    # ...
+    train_data, test_data = train_test_split(ratings_data, test_size=0.2)
+
+    model = SVD()
+    model.fit(train_data)
+
+    predictions = model.test(test_data)
+    mae = accuracy.mae(predictions)
+    print(f"MAE on test set: {mae}")
+
     with open("svd.pkl", "wb") as file:
-        pickle.dump(svd, file)
+        pickle.dump(model, file)
+
+
+ratings = pd.read_csv("Ratings.csv")
+new_ratings = ratings_preprocessing(ratings)
+modeling(new_ratings)
